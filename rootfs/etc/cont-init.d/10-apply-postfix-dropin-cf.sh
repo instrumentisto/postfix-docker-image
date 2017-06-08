04d9f5ca010eb4980bedfc37a85daf5fd057734c
tyranron <tyranron@gmail.com>
2017-06-08 12:30:58 +0300

    Ability to use .postconf drop-in files for master.cf

--- rootfs/etc/cont-init.d/10-apply-postfix-dropin-cf.sh
+++ rootfs/etc/cont-init.d/10-apply-postfix-dropin-cf.sh
@@ -3,23 +3,31 @@
 set -e
 
 
-# Applying Postfix main.cf drop-in files.
+# Applying Postfix main.cf drop-in .cf files.
 # http://www.postfix.org/postconf.5.html
 for file in /etc/postfix/main.cf.d/*.cf; do
   [ -f "$file" ] || continue
-  while read line; do
-    # All valid Postfix main.cf options start with a lower case letter.
-    if (printf "%s" "$line" | grep -qE '^[a-z]'); then
-      postconf -e "$line"
-    fi
-  done < "$file"
+  printf "\n\n#\n# %s\n#\n" "$file" >> /etc/postfix/main.cf
+  cat "$file" >> /etc/postfix/main.cf
 done
 
 
-# Applying Postfix master.cf drop-in files.
+# Applying Postfix master.cf drop-in .cf files.
 # http://www.postfix.org/master.5.html
 for file in /etc/postfix/master.cf.d/*.cf; do
   [ -f "$file" ] || continue
   printf "\n\n#\n# %s\n#\n" "$file" >> /etc/postfix/master.cf
   cat "$file" >> /etc/postfix/master.cf
+done
+
+# Applying Postfix master.cf drop-in .postconf files.
+# http://www.postfix.org/postconf.1.html
+for file in /etc/postfix/master.cf.d/*.postconf; do
+  [ -f "$file" ] || continue
+  while read line; do
+    # All valid master.cf postconf instructions start with one of: -M, -F, -P.
+    if (printf "%s" "$line" | grep -qE '^-[MFP] '); then
+      postconf "$line"
+    fi
+  done < "$file"
 done
